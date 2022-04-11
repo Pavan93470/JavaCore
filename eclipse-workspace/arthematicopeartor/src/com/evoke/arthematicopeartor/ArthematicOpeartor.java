16f718d4e6115efd66fe33bb6fa12b24b43f178d
rbattila <rbattila@RBATTILA-L-2653.evoketechnologies.com>
2022-04-11 17:58:04 +0530

    Testing

---
+++ eclipse-workspace/arthematicopeartor/src/com/evoke/arthematicopeartor/ArthematicOpeartor.java
@@ -0,0 +1,13 @@
+package com.evoke.arthematicopeartor;
+
+public class ArthematicOpeartor {
+	public static void main(String[] args) {
+		int x = 200, y = 300;
+		System.out.println("addition:" + (x + y));
+		System.out.println("subtraction:" + (x - y));
+		System.out.println("multiplication:" + (x * y));
+		System.out.println("division:" + (x / y));
+		System.out.println("remaindar:" + (x % y));
+	}
+
+}
---
+++ eclipse-workspace/arthematicopeartor/src/com/evoke/arthematicopeartor/package-info.java
@@ -0,0 +1,1 @@
+package com.evoke.arthematicopeartor;
